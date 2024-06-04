1fc355aa1e51f65424d4a8d58f6c32029ce1d2c4
Zimparks <tnyoni@zimparks.org.zw>
2024-06-04 10:56:03 +0200

    Location

--- parks_app/screens/location/Location.jsx
+++ parks_app/screens/location/Location.jsx
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView, { Marker } from 'react-native-maps'
 
 const Location = () => {
+
+  const coordinates = {
+    latitude: -17.79958,
+    longitude: 31.05493,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+    title: "My Location"
+  }
   return (
-    <View>
-      <Text>Location</Text>
-    </View>
+    <MapView initialRegion={coordinates} style={styles.mapStyle}>
+      <Marker coordinate={coordinates} title={coordinates.title}/>
+
+    </MapView>
   )
 }
 
 export default Location
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mapStyle: {
+    ...StyleSheet.absoluteFillObject
+  }
+})
